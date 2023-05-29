bd6765204fab94672710cbf8fb7664eb1826b68e
indrakshi111 <132567733+indrakshi111@users.noreply.github.com>
2023-05-30 01:30:39 +0530

    Add files via upload

---
+++ Selenium/Activities/FST_SELENIUM_ACTIVITIES/src/main/java/org/example/Activity1.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Activity1 {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        WebDriverManager.firefoxdriver().setup();
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://training-support.net");
+        System.out.println("The Title is"+ driver.getTitle());
+        driver.findElement(By.id("about-link")).click();
+        // Print the title of the new page
+        System.out.println("About page title: " + driver.getTitle());
+
+        // Close the browser
+        driver.close();
+        }
+    }
